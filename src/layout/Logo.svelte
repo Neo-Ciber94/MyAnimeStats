0fe3b73ea95505c4a086ff22a6b46d9650ed4866
Freddy L <neo-ciber_94@hotmail.com>
2023-07-23 12:12:12 -0400

    minor changes

--- src/layout/Logo.svelte
+++ src/layout/Logo.svelte
@@ -1,7 +1,3 @@
-<script>
-	import GradientText from '../components/GradientText.svelte';
-</script>
-
 <div class="text-white text-4xl font-bold" style="word-spacing: 0em;">
 	<a href="/"> MyAnimeStats </a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,5 @@
-<script lang="ts">
-	import { signIn, signOut, getSessionToken } from '$lib/myanimelist/auth/client';
-	import UserProfile from '$lib/myanimelist/svelte/UserProfile.svelte';
+<script>
+	import Profile from '../components/Profile.svelte';
 </script>
 
-<div class="flex flex-col justify-center items-center my-4 w-full gap-4">
-	{#await getSessionToken()}
-		Loading...
-	{:then sessionToken}
-		{#if sessionToken == null}
-			<button
-				on:click={() => signIn()}
-				class="bg-blue-500 px-10 py-3 hover:bg-blue-600 rounded-lg text-white text-center"
-			>
-				Login
-			</button>
-		{:else}
-			<button
-				on:click={() => signOut()}
-				class="bg-indigo-500 px-10 py-3 hover:bg-indigo-600 rounded-lg text-white text-center"
-			>
-				Logout
-			</button>
-			<UserProfile accessToken={sessionToken.accessToken} />
-		{/if}
-	{:catch err}
-		<code class="text-red-500 italic">
-			<pre>{JSON.stringify(err, null, 2)}</pre>
-		</code>
-	{/await}
-</div>
+<Profile />
