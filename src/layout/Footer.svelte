12dc69b847e2589b31b1af917de65cd6d4a15e02
Freddy L <neo-ciber_94@hotmail.com>
2023-07-27 01:54:56 -0400

    Updated footer

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -1,5 +1,18 @@
-<footer>
-	<div class="flex flex-row gap-4 justify-center items-center min-h-[130px] bg-gray-950 text-white">
-		MyAnimeStats &copy; {new Date().getFullYear()}
+<footer
+	class=" min-h-[130px] bg-gray-950 flex flex-col gap-4 justify-center items-center text-white p-5"
+>
+	<div class="flex flex-row gap-4 items-center">
+		<span>Powered By</span>
+		<a href="https://myanimelist.net/" target="_blank" referrerpolicy="no-referrer">
+			<img
+				class="object-contain w-32 h-10"
+				src="/images/myanimelist-logo-white.png"
+				alt="MyAnimeList Logo"
+			/>
+		</a>
+	</div>
+
+	<div class="text-sm">
+		<span>MyAnimeStats &copy; {new Date().getFullYear()}</span>
 	</div>
 </footer>
