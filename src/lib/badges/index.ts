a4ff5411a5f945e0c9c0f4484f65269662837c6c
Freddy L <neo-ciber_94@hotmail.com>
2023-08-12 19:32:16 -0400

    feat: Added check for unique badges

--- src/lib/badges/index.ts
+++ src/lib/badges/index.ts
@@ -1,11 +1,24 @@
 import type { AnimeBadge } from "./AnimeBadge";
 import genresBadges from "./common/genresBadges";
 import customBadges from "./common/customBadges";
+import { dev } from '$app/environment';
 
 const badges = Object.freeze([
     ...genresBadges,
     ...customBadges,
 ] as AnimeBadge[]);
+
+// Ensure the badge id are unique
+if (dev) {
+    const keys = new Set<string>();
+    for (const badge of badges) {
+        if (keys.has(badge.id)) {
+            throw new Error(`Badge with id '${badge.id}' already exists`);
+        }
+
+        keys.add(badge.id);
+    }
+}
 
 export default badges;
 
