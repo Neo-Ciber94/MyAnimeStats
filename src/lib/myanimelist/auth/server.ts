4bb2204a7075883bea1e42da36fcfb7906944534
Freddy L <neo-ciber_94@hotmail.com>
2023-08-19 14:27:45 -0400

    replaced node:crypto for WebCrypto

--- src/lib/myanimelist/auth/server.ts
+++ src/lib/myanimelist/auth/server.ts
@@ -1,10 +1,9 @@
 import { MY_ANIME_LIST_CLIENT_ID, MY_ANIME_LIST_CLIENT_SECRET } from '$env/static/private';
-import crypto from 'node:crypto';
 import { z } from 'zod';
 import * as jose from 'jose';
 
 const MY_ANIME_LIST_OAUTH2_URL = "https://myanimelist.net/v1/oauth2";
-const CODE_VERIFIER = createCodeVerifier();
+const CODE_VERIFIER = await createCodeVerifier();
 
 export interface GetAuthenticationUrlOptions {
     redirectTo?: string;
@@ -156,20 +155,32 @@ export namespace Auth {
     }
 }
 
-function createCodeVerifier(length = 64) {
+async function createCodeVerifier(length = 64) {
     if (length < 43 || length > 128) {
         throw new Error("code verifier length must be between 43 and 128 characters");
     }
 
-    const characters = crypto.randomBytes(length).toString('ascii');
+    // Generate a random string of the specified length
+    const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+    let codeVerifier = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        codeVerifier += charset.charAt(randomIndex);
+    }
 
-    const base64Digest = crypto
-        .createHash("sha256")
-        .update(characters)
-        .digest("base64");
+    // Convert the code verifier to a Uint8Array
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
 
-    return base64Digest
-        .replace(/\+/g, "-")
-        .replace(/\//g, "_")
-        .replace(/=/g, "");
+    // Calculate the SHA-256 hash of the code verifier
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+
+    // Convert the hash to a base64 URL-safe string
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashBase64 = btoa(String.fromCharCode(...hashArray))
+        .replace('+', '-')
+        .replace('/', '_')
+        .replace(/=+$/, '');
+
+    return hashBase64;
 }
