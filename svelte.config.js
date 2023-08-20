add546f6a4a4f16a1a87874afe314af6d9704c24
Freddy L <neo-ciber_94@hotmail.com>
2023-08-20 15:24:46 -0400

    Updated node polyfill

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 			'$components/*': 'src/components/*',
 			'$assets/*': 'src/assets/*',
 			'@/*': 'src/*'
-		}
+		},
 	},
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
+const nodePolyfill = { ...rollupNodePolyFill({ crypto: true }), name: 'rollup-plugin-node-builtins' }
+
 export default defineConfig({
-	plugins: [sveltekit(), rollupNodePolyFill({ crypto: true })],
+	plugins: [sveltekit(), nodePolyfill],
 	ssr: {
 		noExternal: ['flowbite-svelte', 'flowbite-svelte-icons']
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
+	optimizeDeps: {
+
+	}
 });
