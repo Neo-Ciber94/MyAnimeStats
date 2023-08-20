fa31502961c730ae4b44fa9239d8add2bb50b4a4
Freddy L <neo-ciber_94@hotmail.com>
2023-08-20 14:56:07 -0400

    Added crypto polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
-const nodePolyfillsPlugin = nodePolyfills({
-	globals: {
-		global: true
-	}
-});
-
 export default defineConfig({
-	plugins: [sveltekit(), rollupNodePolyFill()],
+	plugins: [sveltekit(), rollupNodePolyFill({ crypto: true })],
 	ssr: {
 		noExternal: ['flowbite-svelte', 'flowbite-svelte-icons']
 	},
