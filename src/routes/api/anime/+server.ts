8d198a72db506e1e30d607ed42d870cd5a601fdf
Freddy L <neo-ciber_94@hotmail.com>
2023-08-23 19:01:01 -0400

    Updated anime search endpoint

--- src/routes/api/anime/+server.ts
+++ src/routes/api/anime/+server.ts
@@ -1,7 +1,7 @@
 import { MALClient } from "@/lib/myanimelist/api";
 import type { RequestHandler } from "./$types";
 import { PUBLIC_MY_ANIME_LIST_CLIENT_ID } from "$env/static/public";
-import { getCurrentAnimeSeason, type AiringStatus } from "@/lib/myanimelist/common/types";
+import { getCurrentAnimeSeason, type AiringStatus, type AnimeSeason } from "@/lib/myanimelist/common/types";
 import ANIME_GENRES from "@/types/generated/animeGenres.generated";
 import { error } from "@sveltejs/kit";
 
@@ -9,48 +9,72 @@ const LIMIT = 100;
 
 export const GET: RequestHandler = async ({ request }) => {
     const { searchParams } = new URL(request.url);
-    const q = searchParams.get('q') || '';
+    const q = searchParams.get('q')?.trim() || '';
     const offset = parseNumberOrNull(searchParams.get('offset')) ?? 0;
+
+    if (q.length > 0 && q.length < 3) {
+        throw error(400, 'Expected a search of 3 or more characters')
+    }
+
+    // If there is not search params we just return the seasonal anime
+    if (q.length === 0) {
+        const { animeList, next } = await getSeasonalAnime(offset);
+        return Response.json({ data: animeList, next });
+    }
+
+    const { animeList, next } = await getAnimeByQuery({ q, offset });
+    return Response.json({ data: animeList, next });
+}
+
+async function getSeasonalAnime(offset: number) {
+    const malClient = new MALClient({
+        clientId: PUBLIC_MY_ANIME_LIST_CLIENT_ID
+    })
+
+    const { season, year } = getCurrentAnimeSeason();
+    const result = await malClient.getSeasonalAnime({
+        season,
+        year,
+        limit: LIMIT,
+        offset,
+        sort: 'anime_num_list_users',
+        fields: ["nsfw", "genres", "status", "mean", 'start_season', 'broadcast'],
+        nsfw: true
+    });
+
+    const AVAILABLE_STATUSES = ['currently_airing', 'finished_airing'] as AiringStatus[];
+    const animeList = result.data.filter(({ node }) => {
+        if (!node.start_season) {
+            return false;
+        }
+
+        return node.start_season.season === season
+            && node.start_season.year === year
+            && node.broadcast?.start_time != null
+            && AVAILABLE_STATUSES.includes(node.status)
+            && !node.genres.some(x => x.id === ANIME_GENRES.Hentai.ID)
+    });
+
+    return { animeList, next: null }
+}
+
+type AnimeQuery = {
+    q: string,
+    offset: number,
+    season?: AnimeSeason,
+    year?: number,
+    allowNsfw?: boolean,
+}
+
+async function getAnimeByQuery(query: AnimeQuery) {
+    const { q, offset, allowNsfw = false } = query;
 
     const malClient = new MALClient({
         clientId: PUBLIC_MY_ANIME_LIST_CLIENT_ID
     })
 
-    const length = q.trim().length;
-
-    if (length > 0 && length < 3) {
-        throw error(400, 'Expected a search of 3 or more characters')
-    }
-
-    if (length === 0) {
-        const { season, year } = getCurrentAnimeSeason();
-        const result = await malClient.getSeasonalAnime({
-            season,
-            year,
-            limit: LIMIT,
-            offset,
-            sort: 'anime_num_list_users',
-            fields: ["nsfw", "genres", "status", "mean", 'start_season', 'broadcast'],
-            nsfw: true
-        });
-
-        const AVAILABLE_STATUSES = ['currently_airing', 'finished_airing'] as AiringStatus[];
-        const animeList = result.data.filter(({ node }) => {
-            if (!node.start_season) {
-                return false;
-            }
-
-            return node.start_season.season === season
-                && node.start_season.year === year
-                && node.broadcast?.start_time != null
-                && AVAILABLE_STATUSES.includes(node.status)
-                && !node.genres.some(x => x.id === ANIME_GENRES.Hentai.ID)
-        });
-
-        return Response.json({ data: animeList, next: null });
-    }
-
     const result = await malClient.getAnimeList({
+        // sort? https://myanimelist.net/forum/?topicid=1936588
         q,
         limit: LIMIT,
         offset,
@@ -59,10 +83,10 @@ export const GET: RequestHandler = async ({ request }) => {
     })
 
     const animeList = result.data.filter(({ node }) => {
-        return !node.genres?.some(x => x.id === ANIME_GENRES.Hentai.ID)
+        return !allowNsfw && !node.genres?.some(x => x.id === ANIME_GENRES.Hentai.ID)
     });
 
-    return Response.json({ data: animeList, next: null });
+    return { animeList, next: null }
 }
 
 function parseNumberOrNull(s: string | null): number | null {
