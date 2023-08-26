63d0e5c233e676ce11461645e33265b66b885265
Freddy L <neo-ciber_94@hotmail.com>
2023-08-26 00:09:35 -0400

    Added genres to AnimeCard

--- src/components/AiringStatusBadge.svelte
+++ src/components/AiringStatusBadge.svelte
@@ -6,11 +6,11 @@
 </script>
 
 {#if status === 'currently_airing'}
-	<Badge rounded color="green">Airing</Badge>
+	<Badge rounded color="green" class={$$restProps.class}>Airing</Badge>
 {:else if status === 'finished_airing'}
-	<Badge rounded color="yellow">Finished</Badge>
+	<Badge rounded color="yellow" class={$$restProps.class}>Finished</Badge>
 {:else if status === 'not_yet_aired'}
-	<Badge rounded color="red">Not Airing</Badge>
+	<Badge rounded color="red" class={$$restProps.class}>Not Airing</Badge>
 {:else}
-	<Badge rounded>{status}</Badge>
+	<Badge rounded class={$$restProps.class}>{status}</Badge>
 {/if}
--- src/components/AnimeCard.svelte
+++ src/components/AnimeCard.svelte
@@ -8,13 +8,16 @@
 
 <a
 	href={`/anime/${anime.node.id}`}
-	class="shadow-lg rounded-lg border border-violet-950 flex flex-col
-    bg-gray-950 gap-4 px-2 pt-4 pb-2 h-full items-center"
+	class="shadow-sm rounded-lg border border-violet-950 flex flex-col
+	transition-all duration-300 hover:shadow-violet-900 hover:shadow-md
+    bg-gray-950 gap-2 px-2 pt-4 pb-2 h-full items-center"
 >
 	<div class="text-right w-full flex flex-row gap-2 justify-end">
-		<AiringStatusBadge status={anime.node.status} />
+		<AiringStatusBadge status={anime.node.status} class="text-[10px]" />
 		{#if anime.node.mean}
-			<Badge rounded color="purple" class="font-bold">{anime.node.mean.toFixed(2)}</Badge>
+			<Badge rounded color="purple" class="font-bold text-[10px]"
+				>{anime.node.mean.toFixed(2)}</Badge
+			>
 		{/if}
 	</div>
 
@@ -32,4 +35,14 @@
 	<h5 class="mb-2 text-xs text-center font-medium tracking-tight text-white break-all">
 		{anime.node.title}
 	</h5>
+
+	{#if anime.node.genres}
+		<div class="flex flex-row flex-wrap gap-1 justify-center mb-2">
+			{#each anime.node.genres as genre}
+				<div class="px-1 py-[2px] rounded-lg text-white bg-pink-600 text-[10px]">
+					{genre.name}
+				</div>
+			{/each}
+		</div>
+	{/if}
 </a>
