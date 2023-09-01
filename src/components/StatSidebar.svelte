97dc69ca116547cf13dd4ebcd392990b3425eaae
Freddy L <neo-ciber_94@hotmail.com>
2023-08-31 22:37:57 -0400

    Updated sidebar

--- src/components/StatSidebar.svelte
+++ src/components/StatSidebar.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-	class={`w-11/12 overflow-hidden md:w-[300px] shrink-0 bg-gradient-to-tl
+	class={`w-11/12 overflow-hidden md:w-[240px] lg:w-[300px] shrink-0 bg-gradient-to-tl
 		from-indigo-600 from-10 to-indigo-800 mx-auto my-2 md:my-4 md:mx-4 md:min-h-screen shadow-lg 
 		px-4 py-10 flex flex-row md:flex-col items-center rounded-2xl gap-4 md:gap-0`}
 >
@@ -17,7 +17,7 @@
 		</div>
 	{:else if $session.user}
 		<div class="flex flex-col gap-2 md:gap-4 items-center mr-5 md:mr-0">
-			<div class="aspect-square w-24 h-24 md:w-36 md:h-36">
+			<div class="aspect-square w-24 h-24 md:w-28 md:h-28 lg:w-36 lg:h-36">
 				<img
 					class="border-4 md:border-8 border-white/30 shadow-md w-full h-full
 						object-cover overflow-hidden rounded-full"
@@ -36,31 +36,31 @@
 				<span slot="count" class="text-white"
 					>{$session.user.anime_statistics?.num_items_watching ?? 0}</span
 				>
-				<span slot="text" class="text-white text-base md:text-xl">Watching</span>
+				<span slot="text" class="text-white text-base lg:text-xl">Watching</span>
 			</StatIndicator>
 
 			<StatIndicator color="#FF8A40">
 				<span slot="count" class="text-white"
 					>{$session.user.anime_statistics?.num_items_completed ?? 0}</span
 				>
-				<span slot="text" class="text-white text-base md:text-xl">Completed</span>
+				<span slot="text" class="text-white text-base lg:text-xl">Completed</span>
 			</StatIndicator>
 
 			<StatIndicator color="#ffff00">
 				<span slot="count">{$session.user.anime_statistics?.num_items_on_hold ?? 0}</span>
-				<span slot="text" class="text-white text-base md:text-xl">On Hold</span>
+				<span slot="text" class="text-white text-base lg:text-xl">On Hold</span>
 			</StatIndicator>
 
 			<StatIndicator color="#ff0000">
 				<span slot="count" class="text-white"
 					>{$session.user.anime_statistics?.num_items_dropped ?? 0}</span
 				>
-				<span slot="text" class="text-white text-base md:text-xl">Dropped</span>
+				<span slot="text" class="text-white text-base lg:text-xl">Dropped</span>
 			</StatIndicator>
 
 			<StatIndicator color="#cfcfcf">
 				<span slot="count">{$session.user.anime_statistics?.num_items_plan_to_watch ?? 0}</span>
-				<span slot="text" class="text-white text-base md:text-xl">Plan To Watch</span>
+				<span slot="text" class="text-white text-base lg:text-xl">Plan To Watch</span>
 			</StatIndicator>
 		</div>
 	{/if}
