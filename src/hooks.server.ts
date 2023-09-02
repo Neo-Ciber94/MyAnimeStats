ed5876cea1bb8b8883d7291b748f111c24f18348
Freddy L <neo-ciber_94@hotmail.com>
2023-09-01 20:26:54 -0400

    Added middleware api

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,37 +1,10 @@
-import { MyAnimeListHandler } from "$lib/myanimelist/svelte/handle";
 import type { Handle } from "@sveltejs/kit";
-import { getServerSession } from "./lib/myanimelist/svelte/auth";
-import { MALClient } from "./lib/myanimelist/api";
-import { dev } from "$app/environment";
+import { createMiddlewareHandler } from "./lib/middlewares/handle";
 
-const myAnimeListHandler = MyAnimeListHandler();
+const middlewareHandle = createMiddlewareHandler();
 
 export const handle = (async ({ event, resolve }) => {
-    if (dev) {
-		const { fallBackPlatformToMiniFlareInDev } = await import('$lib/miniflare');
-		event.platform = await fallBackPlatformToMiniFlareInDev(event.platform);
-	}
-    
-    const session = await getServerSession(event.cookies);
-
-    if (session) {
-        try {
-            const { accessToken } = session;
-            const malClient = new MALClient({ accessToken });
-            const user = await malClient.getMyUserInfo({ fields: ['anime_statistics'] });
-            event.locals.authenticatedUser = { user, accessToken };
-        }
-        catch (err) {
-            console.error(err);
-        }
-    }
-
-    const pathname = event.url.pathname;
-
-    if (pathname.startsWith("/api/myanimelist")) {
-        return myAnimeListHandler(event);
-    }
-
-    const response = await resolve(event);
+    const response = await middlewareHandle({ event, resolve });
     return response;
 }) satisfies Handle;
+
---
+++ src/lib/middlewares/handle.ts
@@ -0,0 +1,73 @@
+import { dev } from "$app/environment";
+import { combineMiddlewares, type Middleware } from ".";
+import { MALClient } from "../myanimelist/api";
+import { getServerSession } from "../myanimelist/svelte/auth";
+import { MyAnimeListHandler } from "../myanimelist/svelte/handle";
+
+function miniflareMiddleware(): Middleware {
+    return async (event, next) => {
+        if (dev) {
+            const { fallBackPlatformToMiniFlareInDev } = await import('$lib/miniflare');
+            event.platform = await fallBackPlatformToMiniFlareInDev(event.platform);
+        }
+
+        return next(event);
+    }
+}
+
+function myAnimeListMiddleware(): Middleware {
+    return (event, next) => {
+        const myAnimeListHandler = MyAnimeListHandler();
+        const pathname = event.url.pathname;
+
+        if (pathname.startsWith("/api/myanimelist")) {
+            return myAnimeListHandler(event);
+        }
+
+        return next(event);
+    }
+}
+
+function authMiddleware(): Middleware {
+    return async (event, next) => {
+        const session = await getServerSession(event.cookies);
+
+        if (session) {
+            try {
+                const { accessToken } = session;
+                const malClient = new MALClient({ accessToken });
+                const user = await malClient.getMyUserInfo({ fields: ['anime_statistics'] });
+                event.locals.authenticatedUser = { user, accessToken };
+            }
+            catch (err) {
+                console.error(err);
+            }
+        }
+
+        return next(event);
+    }
+}
+
+function loggerMiddleware(): Middleware {
+    return async (event, next) => {
+        const startMs = Date.now();
+        const response = await next(event);
+        const elapsedMs = Date.now() - startMs;
+
+        const icon = response.ok ? "✅" : "❌";
+        const request = event.request;
+        const method = request.method;
+        const status = response.status;
+        const url = request.url;
+        const now = new Date().toISOString();
+        // ❌ 2023-09-01T23:13:48.386Z - GET 200 (230ms) - /api/users/
+        console.log(`${icon} ${now} - ${method} ${status} (${elapsedMs}ms) - ${url}`);
+
+        return response;
+    }
+}
+
+export function createMiddlewareHandler() {
+    const handle = combineMiddlewares([loggerMiddleware(), miniflareMiddleware(), authMiddleware(), myAnimeListMiddleware()]);
+    return handle;
+}
