6f59b60773bcd11df11ddc4717f3ae39e6466338
Freddy L <neo-ciber_94@hotmail.com>
2023-09-07 23:38:58 -0400

    view transitions typings

--- src/global.d.ts
+++ src/global.d.ts
@@ -24,3 +24,29 @@ export global {
      */
     export type Nullable<T> = T | null | undefined;
 }
+
+// View transitions 
+export global {
+
+    export interface CSSStyleDeclaration {
+        /**
+         * The view-transition-name CSS property provides the selected element with a distinct identifying name 
+         * (a <custom-ident>) and causes it to participate in a separate view transition 
+         * from the root view transition — or no view transition if the none value is specified.
+         */
+        viewTransitionName?: string;
+    }
+
+    export interface Document {
+        /**
+         * The startViewTransition() method of the View Transitions API starts a new view transition 
+         * and returns a ViewTransition object to represent it.
+         * 
+         * @param callback A callback function typically invoked to update the DOM during the view transition process, 
+         * which returns a Promise. The callback is invoked once the API has taken a screenshot of the current page. 
+         * When the promise returned by the callback fulfills, the view transition begins in the next frame. 
+         * If the promise returned by the callback rejects, the transition is abandoned.
+         */
+        startViewTransition?(callback: () => Promise<unknown>);
+    }
+}
