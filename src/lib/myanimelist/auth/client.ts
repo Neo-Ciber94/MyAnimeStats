e41bd9c6296830e0c587649726ce9ada8df6edb3
Freddy L <neo-ciber_94@hotmail.com>
2023-09-15 01:51:24 -0400

    docs

--- src/lib/myanimelist/auth/client.ts
+++ src/lib/myanimelist/auth/client.ts
@@ -7,18 +7,36 @@ const userTokenSchema = z.object({
     expiresAt: z.string().pipe(z.coerce.date())
 });
 
+/**
+ * The user token.
+ */
 export type UserToken = z.infer<typeof userTokenSchema>;
 
-
+/**
+ * Redirects to the sign-in page.
+ */
 export function signIn() {
     window.location.href = `${window.location.origin}${getApiUrl()}/auth/sign-in`
 }
 
+/**
+ * Redirects to the sign-out page.
+ */
 export function signOut() {
     window.location.href = `${window.location.origin}${getApiUrl()}/auth/sign-out`
 }
 
-export async function getUserToken(): Promise<UserToken | null> {
+/**
+ * Gets the current user access token.
+ * @param opts Additional options.
+ */
+export async function getUserToken(opts?: {
+    /**
+     * Whether if skip the returned json validation, this is in case
+     * contract is broken, we should never happen.
+     */
+    skipValidation?: boolean
+}): Promise<UserToken | null> {
     const res = await fetch(`${getApiUrl()}/auth/token`);
 
     if (!res.ok) {
@@ -27,14 +45,28 @@ export async function getUserToken(): Promise<UserToken | null> {
         return null;
     }
 
+    // I'm just paranoid, but this never should be necessary
+    if (opts?.skipValidation === true) {
+        return await res.json() as UserToken;
+    }
+
     const userToken = userTokenSchema.parse(await res.json());
     return userToken;
 }
 
-type Session = UserToken & {
+/**
+ * The user session.
+ */
+export type Session = UserToken & {
+    /**
+     * The current user.
+     */
     user: User
 }
 
+/**
+ * Gets the current user session.
+ */
 export async function getSession() {
     const url = `${getApiUrl()}/auth/session`;
     const res = await fetch(`${url}?include_anime_statistics=true`);
--- src/lib/myanimelist/common/getApiUrl.ts
+++ src/lib/myanimelist/common/getApiUrl.ts
@@ -1,3 +1,10 @@
+/**
+ * Returns the path of the `MyAnimeList` api.
+ */
 export function getApiUrl() {
+    if (process.env.MY_ANIME_LIST_API) {
+        return process.env.MY_ANIME_LIST_API;
+    }
+
     return "/api/myanimelist";
 }
