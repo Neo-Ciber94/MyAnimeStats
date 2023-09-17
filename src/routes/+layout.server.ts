258a8987595f8458ac28a6d28a664736aa02ab70
Freddy L <neo-ciber_94@hotmail.com>
2023-09-17 01:36:04 -0400

    minor changes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,5 @@
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
-    if (event.locals.session) {
-        return { session: event.locals.session }
-    }
-
-    return { session: null }
+    return { session: event.locals.session }
 }
