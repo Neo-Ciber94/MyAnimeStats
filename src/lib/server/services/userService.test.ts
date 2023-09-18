4682eadd3f78e923a4985becb495ed82f92e13d9
Freddy L <neo-ciber_94@hotmail.com>
2023-09-17 21:00:58 -0400

    Added method to get user details via scrapping

--- src/lib/server/services/userService.test.ts
+++ src/lib/server/services/userService.test.ts
@@ -6,9 +6,49 @@ describe("Test UserService", () => {
         const result = await UserService.getUserIdFromUsername("freddx");
         expect(result).toStrictEqual(10531632);
     })
-   
+
     test("Should get user id from other username", async () => {
         const result = await UserService.getUserIdFromUsername("ZeroCrystal");
         expect(result).toStrictEqual(5292566);
+    });
+
+    test("Should get user details from own user", async () => {
+        const user = await UserService.getUserDetails("freddx");
+
+        expect(user).toBeTruthy();
+
+        expect(user?.id).toStrictEqual(10531632);
+        expect(user?.name).toBeTruthy();
+        expect(user?.picture).contains("https://");
+
+        expect(user?.anime_statistics?.mean_score).greaterThan(0);
+        expect(user?.anime_statistics?.num_items).greaterThan(0);
+        expect(user?.anime_statistics?.num_episodes).greaterThan(0);
+
+        expect(user?.anime_statistics?.num_items_watching).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_completed).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_dropped).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_on_hold).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_plan_to_watch).greaterThan(0);
+    });
+
+    test("Should get user details from other user", async () => {
+        const user = await UserService.getUserDetails("ZeroCrystal");
+
+        expect(user).toBeTruthy();
+
+        expect(user?.id).toStrictEqual(5292566);
+        expect(user?.name).toBeTruthy();
+        expect(user?.picture).contains("https://");
+
+        expect(user?.anime_statistics?.mean_score).greaterThan(0);
+        expect(user?.anime_statistics?.num_items).greaterThan(0);
+        expect(user?.anime_statistics?.num_episodes).greaterThan(0);
+
+        expect(user?.anime_statistics?.num_items_watching).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_completed).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_dropped).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_on_hold).greaterThan(0);
+        expect(user?.anime_statistics?.num_items_plan_to_watch).greaterThan(0);
     })
 })
--- src/lib/server/services/userService.ts
+++ src/lib/server/services/userService.ts
@@ -1,8 +1,95 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 
-import parse from "node-html-parser";
+import { PLACEHOLDER_IMAGE } from "@/common/constants";
+import type { WatchStatus } from "@/lib/myanimelist/common/types";
+import type { User } from "@/lib/myanimelist/common/user";
+import parse, { HTMLElement } from "node-html-parser";
 
 export namespace UserService {
+    export async function getUserDetails(username: string): Promise<User | null> {
+        const profileUrl = `https://myanimelist.net/profile/${username}`;
+        const res = await fetch(profileUrl);
+
+        if (!res.ok) {
+            return null;
+        }
+
+        try {
+            const html = await res.text();
+            const htmlDocument = parse(html);
+            const commentLink = htmlDocument.querySelector("h2 span.floatRightHeader a");
+
+            if (commentLink == null) {
+                return null;
+            }
+
+            const href = commentLink.getAttribute('href');
+
+            if (href == null) {
+                return null;
+            }
+
+            const idMatches = /id(1?)=(\d+)/.exec(href);
+
+            if (idMatches == null) {
+                return null;
+            }
+
+            const userId = Number(idMatches[2]);
+
+            if (Number.isNaN(userId)) {
+                return null;
+            }
+
+            const userImage = htmlDocument.querySelector(".user-image img")?.getAttribute("src");
+            const joinedAt = htmlDocument.querySelector("span.user-status-data.di-ib.fl-r")?.textContent?.trim();
+            const meanScore = Number(htmlDocument.querySelector(".user-statistics-stats .stat-score .score-label")?.textContent?.trim());
+
+            const getStat = (status: WatchStatus) => {
+                const text = htmlDocument.querySelector(`.stats-status a.anime.${status} + span`)?.textContent?.replaceAll(/[\s,]/g, "");
+                const count = Number(text);
+                return Number.isNaN(count) ? 0 : count;
+            }
+
+            const numWatching = getStat('watching');
+            const numCompleted = getStat('completed')
+            const numOnHold = getStat('on_hold')
+            const numDropped = getStat('dropped')
+            const numPlanToWatch = getStat('plan_to_watch')
+            const statsData = getStatsData(htmlDocument);
+
+            return {
+                id: userId,
+                name: username,
+                picture: userImage ?? PLACEHOLDER_IMAGE,
+                joined_at: joinedAt ?? "",
+                anime_statistics: {
+                    mean_score: Number.isNaN(meanScore) ? 0 : meanScore,
+                    num_items_watching: numWatching,
+                    num_items_completed: numCompleted,
+                    num_items_on_hold: numOnHold,
+                    num_items_dropped: numDropped,
+                    num_items_plan_to_watch: numPlanToWatch,
+                    num_items: statsData.numItems,
+                    num_episodes: statsData.numEpisodes,
+                    num_times_rewatched: statsData.numRewatched,
+
+                    // ignored
+                    num_days_watched: 0,
+                    num_days_watching: 0,
+                    num_days_completed: 0,
+                    num_days_on_hold: 0,
+                    num_days_dropped: 0,
+                    num_days: 0,
+                }
+            }
+        }
+        catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+
     export async function getUserIdFromUsername(username: string) {
         const profileUrl = `https://myanimelist.net/profile/${username}`;
         const res = await fetch(profileUrl);
@@ -39,5 +126,42 @@ export namespace UserService {
             console.error(err);
             return null;
         }
+    }
+
+
+}
+
+function getStatsData(doc: HTMLElement) {
+    const statusElements = doc.querySelectorAll("ul.stats-data.fl-r li");
+    let numItems = 0;
+    let numRewatched = 0;
+    let numEpisodes = 0;
+
+    for (const el of statusElements) {
+        const tag = el.querySelector("span.fl-l")?.textContent?.trim();
+        const num = el.querySelector("span.fl-r")?.textContent?.replaceAll(/[\s,]/g, "");
+        const n = Number(num);
+
+        if (tag && num) {
+            switch (tag) {
+                case "Total Entries":
+                    numItems = Number.isNaN(n) ? 0 : n;
+                    break;
+                case "Rewatched":
+                    numRewatched = Number.isNaN(n) ? 0 : n;
+                    break;
+                case "Episodes":
+                    numEpisodes = Number.isNaN(n) ? 0 : n;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    return {
+        numItems,
+        numRewatched,
+        numEpisodes,
     }
 }
