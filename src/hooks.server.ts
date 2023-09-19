cdbdeea3735eac9fddcfc1a7cad8b070b1ea13e7
Freddy L <neo-ciber_94@hotmail.com>
2023-09-18 20:47:15 -0400

    removed handleError

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,5 @@
-import type { Handle, HandleServerError } from "@sveltejs/kit";
+import type { Handle } from "@sveltejs/kit";
 import { createMiddlewareHandler } from "./lib/server/middlewares";
-import { logger } from "./lib/server/logger";
 
 const middlewareHandle = createMiddlewareHandler();
 
@@ -8,12 +7,3 @@ export const handle = (async ({ event, resolve }) => {
     const response = await middlewareHandle({ event, resolve });
     return response;
 }) satisfies Handle;
-
-export const handleError: HandleServerError = (input) => {
-    const error = input.error?.toString();
-    logger.error({ error });
-
-    return {
-        message: 'Something went wrong',
-    };
-};
