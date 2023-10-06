b90405cc3d3260fc9b5fd38dc55dedd675a0ff26
Freddy L <neo-ciber_94@hotmail.com>
2023-10-06 01:35:56 -0400

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import dotenv from "dotenv";
+dotenv.config();
 
 const nodePolyfill = await loadNodePolyfill();
+
+
+const defineProcessEnv = () => {
+  const definedEnvs = Object.fromEntries(
+    Object.entries(process.env || {}).map(([key, value]) => [
+      `process.env.${key}`,
+      JSON.stringify(value),
+    ])
+  );
+
+  return definedEnvs;
+};
 
 export default defineConfig({
 	ssr: {
@@ -20,7 +34,8 @@ export default defineConfig({
 	define: {
 		__DATE__: `'${new Date().toISOString()}'`,
 		__RELOAD_SW__: false,
-		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"'
+		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"',
+		...defineProcessEnv()
 	},
 	plugins: [
 		nodePolyfill,
