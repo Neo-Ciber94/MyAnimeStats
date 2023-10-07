32129690d84756cdc7ca6b62d21bf5d37694b65d
Freddy L <neo-ciber_94@hotmail.com>
2023-10-07 15:06:22 -0400

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
 		__DATE__: `'${new Date().toISOString()}'`,
 		__RELOAD_SW__: false,
 		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"',
-		process: '{}',
+		'process.env': '{}',
 		...defineProcessEnv()
 	},
 	plugins: [
